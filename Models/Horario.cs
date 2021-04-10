bf2f23a8c2d19e0623689ddd396e94f399e6ef2a
ArcenAndres <71192846+ArcenAndres@users.noreply.github.com>
2021-04-09 19:58:27 -0500

    Add files via upload

---
+++ Models/Horario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HorarioApi.Models
+{
+    public class Horario
+    {
+        public int NroHorario { get; set; }
+        public DateTime DiaMateria { get; set; }
+        public int CicloHorario { get; set; }
+    }
+}
